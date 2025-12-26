e2c45c91703a0ef408455f4cc72b784b5e28605b
Ahmed Boudabous <ahmedboudabous@gmail.com>
2025-12-26 18:31:21 +0100

    Fix profile image import path for Vercel

--- src/components/Accueil/Hero.jsx
+++ src/components/Accueil/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaRocket, FaGithub, FaLinkedin } from "react-icons/fa";
-import profilePhoto from "C:/all react project/ahmed-boudabous/src/assets/profile.png";
+import profile from "../../assets/profile.png";
 
 const Hero = () => {
   const user = {
